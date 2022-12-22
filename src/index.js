1826a70cee26909c7e91dbd91e1efd7be6530f95
HTMLissa <melissa_w1994@yahoo.de>
2022-12-22 13:43:23 -0600

    cleaning up code in index.js  and adding to changeTab function in switchTabs.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import "./style.css";
 import { createLandingPage } from "./initialPageLoad";
 import changeTab from "./switchTabs";
-import { createAboutUsPage } from "./aboutUs";
-import { createRestaurantMenu } from "./menu";
-import { createLocationPage } from "./locationPage";
-import { recommendCheese } from "./cheeseOfTheWeek";
 
 createLandingPage();
 
 // adding functionality
 changeTab();
-
-// add about us section
-const aboutUsBtn = document.querySelector(".about-us-btn");
-aboutUsBtn.addEventListener("click", createAboutUsPage);
-
-// add menu section
-const menuBtn = document.querySelector(".menu-btn");
-menuBtn.addEventListener("click", createRestaurantMenu);
-
-// add location section
-const locationBtn = document.querySelector(".location-btn");
-locationBtn.addEventListener("click", createLocationPage);
-
-// add cheese of the week section
-const recommendCheeseBtn = document.querySelector(".cheese-of-week-btn");
-recommendCheeseBtn.addEventListener("click", recommendCheese);
--- src/switchTabs.js
+++ src/switchTabs.js
@@ -1,3 +1,24 @@
-const changeTab = () => console.log("switched");
+import { createAboutUsPage } from "./aboutUs";
+import { createRestaurantMenu } from "./menu";
+import { createLocationPage } from "./locationPage";
+import { recommendCheese } from "./cheeseOfTheWeek";
+
+const changeTab = () => {
+  // add about us section
+  const aboutUsBtn = document.querySelector(".about-us-btn");
+  aboutUsBtn.addEventListener("click", createAboutUsPage);
+
+  // add menu section
+  const menuBtn = document.querySelector(".menu-btn");
+  menuBtn.addEventListener("click", createRestaurantMenu);
+
+  // add location section
+  const locationBtn = document.querySelector(".location-btn");
+  locationBtn.addEventListener("click", createLocationPage);
+
+  // add cheese of the week section
+  const recommendCheeseBtn = document.querySelector(".cheese-of-week-btn");
+  recommendCheeseBtn.addEventListener("click", recommendCheese);
+};
 
 export default changeTab;
